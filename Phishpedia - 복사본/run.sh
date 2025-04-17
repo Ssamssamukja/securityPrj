f9645bec68e31c2d0e0272011dca2a76a9903927
Ssamssamukja <109636635+Ssamssamukja@users.noreply.github.com>
2025-04-17 13:42:16 +0900

    phishpedia 수정

--- Phishpedia - 복사본/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker build -t phishpedia-docker .
-
-docker run -it --rm \
-  -v $(pwd):/workspace/Phishpedia \
-  -w /workspace/Phishpedia \
-  phishpedia-docker \
-  bash -c "apt update && apt install -y dos2unix && dos2unix setup.sh && source /opt/miniconda/etc/profile.d/conda.sh && bash setup.sh && conda activate phishpedia && python phishpedia.py --folder ./datasets/test_sites"
